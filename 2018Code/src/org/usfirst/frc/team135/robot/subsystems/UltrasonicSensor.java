1b08ad7203b0335115a9c59c72c9853f0280c498
Evan <esmith25@phm.k12.in.us>
2018-01-30 13:33:58 -0500

    Sonar Update

--- 2018Code/src/org/usfirst/frc/team135/robot/subsystems/UltrasonicSensor.java
+++ 2018Code/src/org/usfirst/frc/team135/robot/subsystems/UltrasonicSensor.java
@@ -9,21 +9,18 @@ import edu.wpi.first.wpilibj.*;
  */
 public class UltrasonicSensor extends Subsystem {
 
-   // private static final int RIGHT_SONAR_PING_PORT = 3;
-    //private static final int RIGHT_SONAR_ECHO_PORT = 4;
+    private static final int RIGHT_SONAR_TRIG_PORT = 3;
+    private static final int RIGHT_SONAR_ECHO_PORT = 4;
     
-    private static final int FRONT_SONAR_PING_PORT = 1;
+    private static final int FRONT_SONAR_TRIG_PORT = 1;
     private static final int FRONT_SONAR_ECHO_PORT = 2;
     
-    private static final int LEFT_SONAR_PING_PORT = 6;
+    private static final int LEFT_SONAR_TRIG_PORT = 6;
     private static final int LEFT_SONAR_ECHO_PORT = 5;
-    // 2015: Front Trig 1 Echo 2
-    // 2015: Left Trig 6 Echo 5
     
-    
-    //private Ultrasonic rightSonar = new Ultrasonic(RIGHT_SONAR_PING_PORT, RIGHT_SONAR_ECHO_PORT);
-    private Ultrasonic frontSonar = new Ultrasonic(FRONT_SONAR_PING_PORT, FRONT_SONAR_ECHO_PORT);
-    private Ultrasonic leftSonar = new Ultrasonic(LEFT_SONAR_PING_PORT, LEFT_SONAR_ECHO_PORT);
+    private Ultrasonic rightSonar = new Ultrasonic(RIGHT_SONAR_TRIG_PORT, RIGHT_SONAR_ECHO_PORT);
+    private Ultrasonic frontSonar = new Ultrasonic(FRONT_SONAR_TRIG_PORT, FRONT_SONAR_ECHO_PORT);
+    private Ultrasonic leftSonar = new Ultrasonic(LEFT_SONAR_TRIG_PORT, LEFT_SONAR_ECHO_PORT);
     private static UltrasonicSensor instance;
     
     
@@ -35,13 +32,19 @@ public class UltrasonicSensor extends Subsystem {
     	}
     	return instance;
     }
-
-    	/*public double GetRightSonarValue()
+    
+    public UltrasonicSensor()
+    {
+    	rightSonar.setAutomaticMode(true);
+    	leftSonar.setAutomaticMode(true);
+    	frontSonar.setAutomaticMode(true);
+    }
+    	public double GetRightSonarValue()
     	{
     		double RightSonarDistance = rightSonar.getRangeInches();
     		SmartDashboard.putNumber("Right Sonar Distance: ",RightSonarDistance);
     		return RightSonarDistance;
-    	}*/
+    	}
     	public double GetFrontSonarValue()
     	{
     		double FrontSonarDistance = frontSonar.getRangeInches();
