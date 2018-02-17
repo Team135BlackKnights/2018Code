02a23411f7c782b36fd8f04f5dd3a25abce2d142
robot <robot@LAPTOP-V9L5NB5D>
2018-02-17 16:13:53 -0500

    Right position

--- 2018Code/src/org/usfirst/frc/team135/robot/commands/auton/entrypoints/RightPosition.java
+++ 2018Code/src/org/usfirst/frc/team135/robot/commands/auton/entrypoints/RightPosition.java
@@ -1,28 +1,139 @@
 package org.usfirst.frc.team135.robot.commands.auton.entrypoints;
 
+import org.usfirst.frc.team135.robot.Robot;
+import org.usfirst.frc.team135.robot.commands.auton.groups.MidToSwitch;
+import org.usfirst.frc.team135.robot.commands.auton.groups.SideToNearSwitch;
+import org.usfirst.frc.team135.robot.commands.auton.singles.InitializeAngle;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
 public class RightPosition extends CommandGroup {
 
+	private static final int
+	CLOSE = 1,
+	FAR = 0,
+	INVALID = -1;
     public RightPosition() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
+    	String msg = DriverStation.getInstance().getGameSpecificMessage();
+    	int switchPosition = getSwitchPosition(msg);
+    	int scalePosition = getScalePosition(msg);
+    	
+    	addSequential(new InitializeAngle(180));
+    	
+    	if (switchPosition == INVALID || scalePosition == INVALID)
+    	{
+    		//no point in running anything but autoline if we don't know where anything is
+    	}
+    	
+    	
+    	if (SmartDashboard.getBoolean("Try to go for Switch?", true) && !SmartDashboard.getBoolean("Try to go for Scale?", false))
+    	{
+    		
+    		addSequential(new SideToNearSwitch());
+    		//Go for switch only
+    		if (switchPosition == CLOSE)
+    		{
+    			//3 cube
+    		}
+    		else
+    		{
+    			//2 cube
+    		}
+    	}
+    	else if (!SmartDashboard.getBoolean("Try to go for Switch?", true) && SmartDashboard.getBoolean("Try to go for Scale?", false))
+    	{
+    		//Go for scale only
+    		if (scalePosition == CLOSE)
+    		{
+    			//2 cube
+    		}
+    		else
+    		{
+    			//1 cube
+    		}
+    		
+    	}
+    	else
+    	{
+    		//Either you want it all or are extremely indecive and didn't select anything. Either way here's a balanced auto
+    		
+    		if (switchPosition == CLOSE && scalePosition == CLOSE)
+    		{
+    			//1 on switch, 1 on scale
+    		}
+    		else if (switchPosition == FAR && scalePosition == FAR)
+    		{
+    			if (SmartDashboard.getBoolean("Tiebreak with: Switch (Check)/Scale (Uncheck)", true))
+    			{
+    				//1 cube switch
+    			}
+    			else
+    			{
+    				//1 cube scale
+    			}
+    		}
+    		else if (switchPosition == CLOSE && scalePosition == FAR)
+    		{
+    			if (SmartDashboard.getBoolean("Tiebreak with: Switch (Check)/Scale (Uncheck)", true))
+    			{
+    				//3 cube switch
+    			}
+    			else
+    			{
+    				//1 cube scale
+    			}
+    		}
+    		else if (switchPosition == FAR && scalePosition == CLOSE)
+    		{
+    			if (SmartDashboard.getBoolean("Tiebreak with: Switch (Check)/Scale (Uncheck)", true))
+    			{
+    				//1 cube switch
+    			}
+    			else
+    			{
+    				//2 cube scale
+    			}
+    		}
+    	}
 
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+    }
+    
+    private int getSwitchPosition(String msg)
+    {
+    	if (msg.toUpperCase().charAt(0) == 'R') //Switch is straight up from us
+    	{
+    		return CLOSE;
+    	}
+    	else if (msg.toUpperCase().charAt(0)  == 'L') //Switch is far away from us
+    	{
+    		return FAR;
+    	}
+    	else
+    	{
+    		System.out.println("Unable to get a valid game specific message. Only running autoline.");
+    		return INVALID;
+    	}
+    }
+    
+    private int getScalePosition(String msg)
+    {
+    	if (msg.toUpperCase().charAt(1)  == 'R') //Switch is straight up from us
+    	{
+    		return CLOSE;
+    	}
+    	else if (msg.toUpperCase().charAt(1)  == 'L') //Switch is far away from us
+    	{
+    		return FAR;
+    	}
+    	else
+    	{
+    		System.out.println("Unable to get a valid game specific message. Only running autoline.");
+    		return INVALID;
+    	}
     }
 }
