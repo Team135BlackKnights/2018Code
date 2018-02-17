dcbcaa7372f264c9f616d9abab93cc7cc9e4ae0b
robot <robot@LAPTOP-V9L5NB5D>
2018-02-17 16:01:10 -0500

    Robot map update

--- 2018Code/src/org/usfirst/frc/team135/robot/RobotMap.java
+++ 2018Code/src/org/usfirst/frc/team135/robot/RobotMap.java
@@ -1,4 +1,5 @@
 /*----------------------------------------------------------------------------*/
+
 /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
@@ -10,15 +11,105 @@ package org.usfirst.frc.team135.robot;
 
 public interface RobotMap 
 {
+	public interface RETURNS
+	{
+		public static final int STRAFE_DRIVE_INVALID = -2;
+	}
+	
+	public interface DIRECTION
+	{
+		public static final int
+			FORWARD = 1,
+			BACKWARD = -1,
+			RIGHT = 1,
+			LEFT = -1,
+			CLOCKWISE = 1,
+			COUNTERCLOCKWISE = -1,
+			NEUTRAL = 0;
+	}
+/*
+	public enum SENSORS
+	{
+		FRONT(0),
+		RIGHT(1),
+		REAR(2),
+		LEFT(3);
+		
+	
+		private int num;
+		
+		private SENSORS(int num)
+		{
+			this.num = num;
+		}
+		
+		public int getFieldBased()
+		{
+			return this.num;
+		}
+		
+		public static void rotate(int times, int direction)
+		{
+			//Will perform a 90 degree rotation to the sensor positions j times
+			
+			for (int j = 0; j < times; j++)
+			{
+				for (int i = 0; i < SENSORS.values().length; i++)
+				{
+					if ((SENSORS.values()[i].num += direction) < 3 && (SENSORS.values()[i].num += direction) > 0)
+					{
+						SENSORS.values()[i].num += direction;
+					}
+					else if ((SENSORS.values()[i].num += direction) == 3 && direction == 1)
+					{
+						SENSORS.values()[i].num = 0;
+					}
+					else if ((SENSORS.values()[i].num += direction) == 0 && direction == -1)
+					{
+						SENSORS.values()[i].num = 3;
+					}
+				}
+			}
+		}
+	}
+	
+	*/
+	public interface FIELD
+	{
+		public static final float //All measurements are in inches
+			AUTO_LINE = 135,
+			WALL_SLANT_END = 10,
+			SCALE_X = 42.5f,
+			SCALE_Y = 248,
+			SIDE_SWITCH_X = 54.5f,
+			SIDE_SWITCH_Y = 165,
+			MID_SWITCH_X = 107.0f,
+			MID_SWITCH_Y = 142.5f;
+			
+		
+	}
+	
 	public interface CANIFIER
 	{
-		public static final int ID = 0;
+		public static final int ID = 1;
 	}
 	
 	public interface LIFT
 	{
 		public static final int 
 			LIFT_MOTOR_ID = 3;
+		
+		public static final double
+			LOW_POSITION = 0,
+			SWITCH_POSITION = 500,
+			SCALE_POSITION = 1310;
+		
+		public static final double
+			kP = 4.5,
+			kI = 0.045,
+			kD = 15,
+			kF = .2;
+			
 	}
 	
 	public interface DRIVETRAIN
