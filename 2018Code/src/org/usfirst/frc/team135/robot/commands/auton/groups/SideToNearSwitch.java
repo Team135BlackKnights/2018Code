3151b9072a136c831cd38fa5c8cdc9282033612c
robot <robot@LAPTOP-V9L5NB5D>
2018-02-17 16:09:15 -0500

    Side to Near Switch

--- 2018Code/src/org/usfirst/frc/team135/robot/commands/auton/groups/SideToNearSwitch.java
+++ 2018Code/src/org/usfirst/frc/team135/robot/commands/auton/groups/SideToNearSwitch.java
@@ -1,28 +1,45 @@
 package org.usfirst.frc.team135.robot.commands.auton.groups;
 
+import org.usfirst.frc.team135.robot.Robot;
+import org.usfirst.frc.team135.robot.RobotMap;
+import org.usfirst.frc.team135.robot.RobotMap.*;
+import org.usfirst.frc.team135.robot.commands.auton.singles.DriveStraightForwardDistance;
+import org.usfirst.frc.team135.robot.commands.auton.singles.SetLiftPosition;
+import org.usfirst.frc.team135.robot.commands.teleop.DriveMandibleWheels;
+import org.usfirst.frc.team135.robot.commands.teleop.ExtendMandibles;
+import org.usfirst.frc.team135.robot.commands.teleop.GrabMandibles;
+import org.usfirst.frc.team135.robot.commands.teleop.ReleaseMandibles;
+import org.usfirst.frc.team135.robot.commands.teleop.RetractMandibles;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
  *
  */
-public class SideToNearSwitch extends CommandGroup {
+public class SideToNearSwitch extends CommandGroup implements RobotMap {
 
-    public SideToNearSwitch() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
+	private boolean done = false;
+    public SideToNearSwitch() 
+    {
 
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
+        
+        
+       	addSequential(new DriveStraightForwardDistance(
+       			0, .5, 5, () -> Robot.ultrasonic.getLeftSonarValue(), false,
+       			FIELD.SIDE_SWITCH_Y, 1.0, 82, () -> Robot.canifier.getFrontLidarInches(), true,
+       			2.0));
+       	
+    	addSequential(new ExtendMandibles());
+       	addSequential(new SetLiftPosition(LIFT.SWITCH_POSITION));
+       	
+       	addSequential(new DriveStraightForwardDistance(
+       			FIELD.SIDE_SWITCH_X, .5, 5, () -> Robot.ultrasonic.getLeftSonarValue(), true,
+       			Robot.canifier.getRearLidarInches(), 1.0,82,  () -> Robot.canifier.getFrontLidarInches(), false,
+       			2.0));
+       	
+       	
+       	addSequential(new GrabMandibles());
 
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+    	
     }
 }
