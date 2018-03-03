b6116ba9507dbe3edcb77451e10a6d443e83ae98
robot <robot@10.102.32.104>
2018-03-03 10:51:59 -0500

    Max ACCL.

---
+++ 2018Code/src/org/usfirst/frc/team135/robot/commands/teleop/ResetNavX.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team135.robot.commands.teleop;
+
+import org.usfirst.frc.team135.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+
+/**
+ *
+ */
+public class ResetNavX extends InstantCommand {
+
+    public ResetNavX() {
+        super();
+    }
+
+    // Called once when the command executes
+    protected void initialize() {
+    	
+    }
+
+}
